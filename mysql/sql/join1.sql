a041f88e44adfd7cdcdf508d41bebde491dd54e1
ezen <ezen@DESKTOP-O03MT84>
2022-08-08 17:35:08 +0900

    1

--- mysql/sql/join1.sql
+++ mysql/sql/join1.sql
@@ -1,10 +1,7 @@
 use lasldjf;
 
-select *from member_address;
-
-
 select
-	b.group_seq
+	a.seq
     ,a.groupName
     ,b.seq
     ,b.codename
@@ -14,31 +11,60 @@ inner join code b on b.group_seq = a.seq
 -- join infrMemberAddress b on b.ifmmSeq = a.ifmmSeq
 ;
 
--- login
-
-
-
-
+-- 상품정보
 select
-	a.seq
+	b.seq
     ,a.title
     ,a.price
     ,a.stock
     ,b.option
+	,b.option_sub
 from product a
 -- left join infrMemberAddress b on b.ifmmSeq = a.ifmmSeq
 inner join product_option b on b.product_seq = a.seq
 -- join infrMemberAddress b on b.ifmmSeq = a.ifmmSeq
+
+;
+
+-- 회원정보
+select
+	a.id
+    ,a.pw
+    ,a.gender
+    ,a.dob
+    ,b.address_zipcode
+    ,b.address_zip
+    ,b.address_detail
+	,a.email
+    ,a.number_phone
+    ,a.Regdate
+    ,b.useYn
+from member a
+-- left join infrMemberAddress b on b.ifmmSeq = a.ifmmSeq
+inner join member_address b on b.member_seq = a.seq
+-- join infrMemberAddress b on b.ifmmSeq = a.ifmmSeq
+;
+
+-- 주문정보
+select
+	a.seq
+    ,b.id
+    ,e.title
+    ,e.price
+    ,b.number_phone
+    ,c.address_zipcode
+    ,c.address_zip
+    ,c.address_detail
+    ,d.payment_way
+from lasldjf.order a
+inner join member b on b.seq = a.seq
+inner join member_address c on c.seq = a.seq
+inner join payment d on d.seq = a.seq
+inner join product_option e on e.seq = a.seq
 ;
 
 
 
-
-
-
-
-select * from lasldjf.member where id = "lasldjf" 
-;
 
 
 -- 무슨 목록
