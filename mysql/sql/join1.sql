8c806d7ae191e033ab20a4e42d71200894874ea8
ezen <ezen@DESKTOP-O03MT84>
2022-08-16 17:08:48 +0900

    1

--- mysql/sql/join1.sql
+++ mysql/sql/join1.sql
@@ -1,17 +1,52 @@
 use lasldjf;
 
+
+-- 공통코드
 select
 	a.seq
     ,a.groupName
     ,b.seq
     ,b.codename
 from group_a a
--- left join infrMemberAddress b on b.ifmmSeq = a.ifmmSeq
 inner join code b on b.group_seq = a.seq
--- join infrMemberAddress b on b.ifmmSeq = a.ifmmSeq
 ;
 
--- 상품정보
+
+-- 로그인
+select 1=1
+	,a.id
+    ,a.pw
+    ,a.delYn
+from member a
+where id = "lasldjf" and pw = "la381"
+and delYn = 1
+;
+
+-- main.keyboard 목록
+select
+	a.title
+    ,a.price
+    ,a.mainYn
+from product a
+where mainYn = 1
+and delYn = 1
+;
+
+-- 
+
+
+
+-- 리뷰
+select
+    a.id
+    ,c.title
+    ,b.content
+from member a
+inner join review b on b.seq = a.seq
+inner join product c on c.seq = a.seq
+
+;
+-- 상품
 select
 	b.seq
     ,a.title
@@ -20,9 +55,7 @@ select
     ,b.option
 	,b.option_sub
 from product a
--- left join infrMemberAddress b on b.ifmmSeq = a.ifmmSeq
 inner join product_option b on b.product_seq = a.seq
--- join infrMemberAddress b on b.ifmmSeq = a.ifmmSeq
 
 ;
 
@@ -40,9 +73,7 @@ select
     ,a.Regdate
     ,b.useYn
 from member a
--- left join infrMemberAddress b on b.ifmmSeq = a.ifmmSeq
 inner join member_address b on b.member_seq = a.seq
--- join infrMemberAddress b on b.ifmmSeq = a.ifmmSeq
 ;
 
 -- 주문정보
@@ -50,6 +81,8 @@ select
 	a.seq
     ,b.id
     ,e.title
+    ,f.option
+    ,f.option_sub
     ,e.price
     ,b.number_phone
     ,c.address_zipcode
@@ -60,18 +93,14 @@ from lasldjf.order a
 inner join member b on b.seq = a.seq
 inner join member_address c on c.seq = a.seq
 inner join payment d on d.seq = a.seq
-inner join product_option e on e.seq = a.seq
+inner join product e on e.seq = a.seq
+inner join product_option f on f.seq = a.seq
 ;
 
 
 
 
 
--- 무슨 목록
-
--- 코멘트 목록
-
--- 극장 목록
 
 
 
