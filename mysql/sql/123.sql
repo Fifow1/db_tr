cad760f008807e487535c732d6773d0ce0dd32a6
ezen <ezen@DESKTOP-O03MT84>
2022-09-27 17:40:44 +0900

    q

--- mysql/sql/123.sql
+++ mysql/sql/123.sql
@@ -82,30 +82,54 @@ ORDER BY CCG.seq ASC
 LIMIT 10 offset 20
 ;
 
-SELECT * FROM code;
-
-
-SELECT count(*) codeGroup;
-
-SELECT
-	count(*)
-FROM  codeGroup CCG;
 
 
 
-SELECT
-	*
-FROM member a
-WHERE 1=1
-;
-
-
+			
+select aa.* FROM(
+	SELECT
+		CCG.seq
+		,CCG.groupName_code
+		,CCG.groupName
+		,CCG.groupName_en
+		,(select count(CC.seq) from code CC where 1=1 and group_seq = CCG.seq) as CCcount
+		,CCG.regdate
+	FROM codeGroup CCG
+	WHERE 1=1
+		<choose>
+			<when test="shDelYn == 2"></when>
+			<when test="shDelYn != null and !shDelYn.equals('')">AND CCG.delYn = #{shDelYn}</when>
+			<otherwise></otherwise>
+		</choose>
+		<choose>
+			<when test="shOption == 1">AND CCG.groupName_code LIKE CONCAT('%',#{shValue},'%')</when>
+			<when test="shOption == 2">AND CCG.groupName LIKE CONCAT('%',#{shValue},'%')</when>
+			<when test="shOption == 3">AND CCG.groupName_en LIKE CONCAT('%',#{shValue},'%')</when>
+			<otherwise></otherwise>
+		</choose>
+		<choose>
+			<when test="shdate == 1">AND CCG.regdate between #{shdate_s} and #{shdate_e}</when>
+			<when test="shdate == 2">AND CCG.moddate between #{shdate_s} and #{shdate_e}</when>
+			<otherwise></otherwise>
+		</choose>
+	ORDER BY CCG.seq ASC
+	) aa
+	limit #{rowNumToShow} offset #{startRnumForMysql}
+</select>;
 SELECT 
-	seq
-	,codeName
-	,group_seq
-FROM
-	code
-WHERE 1=1
-	AND delYn = 1
-;
+	seq as CCGseq
+	,groupName as CCGname
+FROM codeGroup
+WHERE 1=1;
+
+-- 로그인 --
+SELECT seq,id, pw
+POINT FROM member 
+where id = 'lasldjf' AND pw = 'la381';
+
+-- 로그인(비밀번호 암호화) --
+
+SELECT seq,id,pw
+FROM member 
+where id = 'lasldjf';
+
